9bb378870945c5714b3a443a3cd4904d2f39483a
Florentin Bürvenich <f.buervenich@wzl.rwth-aachen.de>
2020-03-25 18:23:35 +0100

    UPDATE proxy configuration and public path
    The proxy to a different Backend does work now.

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -1,39 +1,17 @@
 // vue.config.js
 module.exports = {
+    /**
+     *  "/vuewebapp/" in dev mode, "/"  in production mode
+     *   WARNING:  Do NOT change the public path and proxy configuration
+     *             unless you know what you are doing! Otherwise the
+     *             Backend <-> Frontend connection might not work. 
+    */
+    publicPath: process.env.NODE_ENV === 'production' ? "/" : "/vuewebapp/",
     devServer: {
         proxy: {
-            //     '^/': {
-            //         pathRewrite: {'/' : '/vuewebapp'},
-            //         target: "http://localhost:8080",
-            //         secure: false,
-            //         changeOrigin: true,
-            //         logLevel: "debug",
-            //         cookieDomainRewrite: "localhost",
-            //         onProxyReq: (proxyReq, req, res, options) => {
-            //             // console.log(proxyReq);
-            //             // console.log(req);
-            //             // console.log(res);
-            //             // console.log(options);
-            //             // // proxyReq.setHeader('Cookie', cookie);
-            //             // proxyReq.
-            //         },
-            // onProxyRes: (proxyRes) => {
-            //     // Object.keys(proxyRes.headers).forEach((key) => {
-            //     //     if (key === 'set-cookie' && proxyRes.headers[key]) {
-            //     //         console.log(key + " => " + proxyRes.headers[key])
-            //     //         const cookieTokens = proxyRes.headers[key].split(';');
-            //     //         cookie = cookieTokens.filter(element => element.includes('JSESSIONID')).join(';');
-            //     //     }
-            //     // });
-            // },
-            //     },
-
             '/*': {
                 target: "http://localhost:8080",
-                // pathRewrite: { '^/': '/vuewebapp/' },
                 logLevel: 'debug',
-                // changeOrigin: false,
-                // followRedirects: true,
             }
         }
     }
