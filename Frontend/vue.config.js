2caf82e13b9895ab9b29fe90e50c0fb44df46e1e
Florentin Bürvenich <f.buervenich@wzl.rwth-aachen.de>
2020-03-25 01:23:39 +0100

    UPDATE proxy config (die leider immer noch nicht fuktioniert...)(

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -2,11 +2,38 @@
 module.exports = {
     devServer: {
         proxy: {
-            '^/api': {
-                target: "http://localhost:8080/vuewebapp",
+            //     '^/': {
+            //         pathRewrite: {'/' : '/vuewebapp'},
+            //         target: "http://localhost:8080",
+            //         secure: false,
+            //         changeOrigin: true,
+            //         logLevel: "debug",
+            //         cookieDomainRewrite: "localhost",
+            //         onProxyReq: (proxyReq, req, res, options) => {
+            //             // console.log(proxyReq);
+            //             // console.log(req);
+            //             // console.log(res);
+            //             // console.log(options);
+            //             // // proxyReq.setHeader('Cookie', cookie);
+            //             // proxyReq.
+            //         },
+            // onProxyRes: (proxyRes) => {
+            //     // Object.keys(proxyRes.headers).forEach((key) => {
+            //     //     if (key === 'set-cookie' && proxyRes.headers[key]) {
+            //     //         console.log(key + " => " + proxyRes.headers[key])
+            //     //         const cookieTokens = proxyRes.headers[key].split(';');
+            //     //         cookie = cookieTokens.filter(element => element.includes('JSESSIONID')).join(';');
+            //     //     }
+            //     // });
+            // },
+            //     },
+
+            '/*': {
+                target: "http://localhost:8080",
+                // pathRewrite: { '^/': '/vuewebapp/' },
                 logLevel: 'debug',
-                changeOrigin: true,
-                cookieDomainRewrite: "localhost"
+                // changeOrigin: false,
+                // followRedirects: true,
             }
         }
     }
