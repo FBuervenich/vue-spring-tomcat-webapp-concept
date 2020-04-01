5ec9915be07083737892ab685573c1b7ccc04aa3
Florentin Bürvenich <f.buervenich@wzl.rwth-aachen.de>
2020-04-01 17:47:04 +0200

    ADD functionality to load all greetings

--- Frontend/src/components/GreetingGetAllBox.vue
+++ Frontend/src/components/GreetingGetAllBox.vue
@@ -2,10 +2,18 @@
   <div class="wzl-box" id="get-all">
     <h1>GET-Request ohne Parameter</h1>
     <table class="wzl-table" id="greetings">
-      <tr>
-        <th>id</th>
-        <th>content</th>
-      </tr>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>content</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="greeting in greetings" :key="greeting.id">
+          <td>{{ greeting.id }}</td>
+          <td>{{ greeting.content }}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
@@ -13,7 +21,26 @@
 <script>
 export default {
   name: "GreetingGetAllBox",
-  props: {}
+  props: {},
+  data: function() {
+    return {
+      greetings: []
+    };
+  },
+  created: function() {
+    this.reloadGreetings();
+  },
+  methods: {
+    reloadGreetings: function() {
+      fetch("greetings", { method: "GET" })
+        .then(response => {
+          return response.json();
+        })
+        .then(json => {
+          this.greetings = json;
+        });
+    }
+  }
 };
 </script>
 
